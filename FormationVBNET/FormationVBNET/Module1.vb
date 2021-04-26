a8ffc4897de191230b7dfd2e12a0922ba4f1cda4
cyrilvincent <contact@cyrilvincent.com>
2021-04-26 13:43:43 +0200

    chauvin

--- FormationVBNET/FormationVBNET/Module1.vb
+++ FormationVBNET/FormationVBNET/Module1.vb
@@ -18,29 +18,14 @@
             anneeNaissance = Console.ReadLine()
             Dim age As Integer = anneeNow - anneeNaissance
             Console.WriteLine("Vous avez " & age & " ans")
-            If age Mod 2 = 0 Then
-                Console.WriteLine("Age pair")
-            Else
-                Console.WriteLine("Age impair")
-            End If
-            If age < 1 Then
-                Console.WriteLine("Nourisson")
-            ElseIf age < 12 Then
-                Console.WriteLine("Enfant")
-            ElseIf age < 18 Then
-                Console.WriteLine("Adolescent")
-            ElseIf age < 60 Then
-                Console.WriteLine("Adulte")
-            ElseIf age < 100 Then
-                Console.WriteLine("Retraité")
-            Else
-                Console.WriteLine("Centenaire")
-            End If
+            PrintClasseAge(age)
         End While
 
         For i = 100 To 0 Step -2
             'Exit for
         Next
+
+        Dim result = Add(2, 3)
 
         Console.ReadKey()
 
@@ -56,5 +41,31 @@
         'Mettre la saisie dans une boucle qui se termine quand on saisie 0
 
     End Sub
+
+    Sub PrintClasseAge(age As Integer)
+        If age Mod 2 = 0 Then
+            Console.WriteLine("Age pair")
+        Else
+            Console.WriteLine("Age impair")
+        End If
+        If age < 1 Then
+            Console.WriteLine("Nourisson")
+        ElseIf age < 12 Then
+            Console.WriteLine("Enfant")
+        ElseIf age < 18 Then
+            Console.WriteLine("Adolescent")
+        ElseIf age < 60 Then
+            Console.WriteLine("Adulte")
+        ElseIf age < 100 Then
+            Console.WriteLine("Retraité")
+        Else
+            Console.WriteLine("Centenaire")
+        End If
+    End Sub
+
+    Function Add(i As Integer, j As Integer)
+        Dim k = i + j
+        Return k
+    End Function
 
 End Module
