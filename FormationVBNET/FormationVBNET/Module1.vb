f149db7415379478b1ef186dc5bf773a98553f5b
cyrilvincent <contact@cyrilvincent.com>
2021-04-26 10:47:04 +0200

    chauvin

---
+++ FormationVBNET/FormationVBNET/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hello World!")
+        Dim myFloat As Single = 3.99
+        Dim var As Integer = myFloat
+        Console.WriteLine(var)
+        var = var + 1
+        Console.WriteLine(var)
+        Dim s1 As String = "Visual"
+        Dim s2 As String = "Basic"
+        Dim s3 As String = s1 + " " + s2
+        Console.WriteLine(s3)
+        Console.Write("Saisir une data: ")
+        Dim input As Integer = Console.ReadLine()
+
+        Console.WriteLine(input + 1)
+        Console.ReadKey()
+
+        'TP1
+        'Saisir une année
+        'DateTime.Now().Year va donner l'année actuelle
+        'Afficher l'age de la personne
+
+
+    End Sub
+
+End Module
