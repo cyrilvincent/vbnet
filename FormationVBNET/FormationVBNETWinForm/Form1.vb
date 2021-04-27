6fa1528447373bdb8c55e4eecd977bfe4cc21881
cyrilvincent <contact@cyrilvincent.com>
2021-04-27 10:22:35 +0200

    Chauvin

---
+++ FormationVBNET/FormationVBNETWinForm/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MenuStrip1.ItemClicked
+        ' Créer une Form avec un menu
+        ' Mettre une toolbar
+        ' Mettre un split container
+        ' Mettre à gauche une listbox
+        ' MEttre un split container horizontal à droite
+        ' Dans le panel en haut à droite mettre 2 textobxs, 1 bouton et 3 labels
+        ' Créer un outils qui saisit 2 chiffres puis en appuyant sur le bouton affiche la puissance de l'un par l'autre
+
+
+    End Sub
+End Class
